1a0f6629111c152d26bf5bb5a7cded0677d8b4ad
Max Provin <maxprovin@Maxs-MacBook-Pro-2.local>
2016-11-25 16:25:25 +0000

    we can get chicken recipes!!!!!!!

--- bot.rb
+++ bot.rb
@@ -1,14 +1,22 @@
+require 'dotenv'
+Dotenv.load
 require 'telegram/bot'
-
-token = '272724728:AAH0evtzxHuXfmjNnTtTqt2whPEZ3AUSgok'
+require 'httparty'
+url = "https://api.edamam.com/search?q=chicken&app_id="+ENV['EDAMAM_APP_ID']+"&app_key="+ENV['EDAMAM_APP_KEY']+"&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
+token = ENV['AUTH_TOKEN']
+print h = HTTParty.get(url)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/chickenrecipes'
+      url = "https://api.edamam.com/search?q=chicken&app_id="+ENV['EDAMAM_APP_ID']+"&app_key="+ENV['EDAMAM_APP_KEY']+"&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
+    	HTTParty.get(url)
+    	text = h['hits'].each do |hit| 
+    		text = hit['recipe']['url']
+    		bot.api.send_message(chat_id: message.chat.id, text: text)
+    	end
     end
   end
 end
+
