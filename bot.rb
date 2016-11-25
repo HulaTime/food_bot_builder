ea45781ea956cd081563031a5492fdecb1c8a9e9
Max Provin <maxprovin@Maxs-MacBook-Pro-2.local>
2016-11-25 14:54:01 +0000

    included telegram-bot-ruby gem

---
+++ bot.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '272724728:AAH0evtzxHuXfmjNnTtTqt2whPEZ3AUSgok'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
