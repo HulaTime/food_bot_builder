1ca6d95d5f7acd437d98cf3750d487136e7de97e
Max Provin <maxprovin@Maxs-MacBook-Pro-2.local>
2016-11-25 16:35:09 +0000

    ongoing

--- bot.rb
+++ bot.rb
@@ -4,10 +4,10 @@ require 'telegram/bot'
 require 'httparty'
 url = "https://api.edamam.com/search?q=chicken&app_id="+ENV['EDAMAM_APP_ID']+"&app_key="+ENV['EDAMAM_APP_KEY']+"&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
 token = ENV['AUTH_TOKEN']
-print h = HTTParty.get(url)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+  	print message
     case message.text
     when '/chickenrecipes'
       url = "https://api.edamam.com/search?q=chicken&app_id="+ENV['EDAMAM_APP_ID']+"&app_key="+ENV['EDAMAM_APP_KEY']+"&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
