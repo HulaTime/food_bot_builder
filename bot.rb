1bbe54f0c14a76a19759f3ff30282d4d9c345085
MaxProvin <maxprovin01@gmail.com>
2016-11-26 14:40:57 +0000

    removed all references to auth tok and reconstructed query string

--- bot.rb
+++ bot.rb
@@ -3,31 +3,36 @@ Dotenv.load
 require 'telegram/bot'
 require 'httparty'
 
-t_url = "https://api.telegram.org/bot264986474:AAEFhtMdxFb-4e5ysvWfXNtTAGLnUF1exfQ/getupdates"
-token = ENV['AUTH_TOKEN']
+T_URL = "https://api.telegram.org/bot"+ENV['AUTH_TOKEN']+"/getupdates"
+F_URL = "https://api.edamam.com/search?app_id="+ENV['FOOD_ID']+"&app_key="+ENV['FOOD_KEY']
 
-Telegram::Bot::Client.run(token) do |bot|
+Telegram::Bot::Client.run(ENV['AUTH_TOKEN']) do |bot|
   bot.listen do |message|
-    
     case message.text
 
+    when '/start'
+      bot.api.send_message(chat_id:message.chat.id, text: "Hello, #{message.from.first_name}")
+    
     when '/recipes'
-
-    	updates = HTTParty.get(t_url)
-    	count = updates['result'].length
-    	loop do
-    		if HTTParty.get(t_url)['result'].length != count
-    			url = "https://api.edamam.com/search?q=chicken&app_id="+ENV['EDAMAM_APP_ID']+"&app_key="+ENV['EDAMAM_APP_KEY']+"&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
-    			h = HTTParty.get(url)
-    			text = h['hits'].each do |hit| 
-    				text = hit['recipe']['url']
-    				bot.api.send_message(chat_id: message.chat.id, text: text)
-    			end
-    			break
-    		end
-    	end
-    end
-  end	
+  	  updates = HTTParty.get(T_URL)
+  	  count = updates['result'].length
+  	  bot.api.send_message(chat_id: message.chat.id, text: count)
+      loop do
+  		  if HTTParty.get(T_URL)['result'].length != count - 1
+  			  bot.api.send_message(chat_id: message.chat.id, text: (HTTParty.get(T_URL)['result'].length))
+          ingredient = HTTParty.get(T_URL)['result'][-1]['message']['text']
+          bot.api.send_message(chat_id: message.chat.id, text: ingredient)
+          # h = HTTParty.get((F_URL+"&q=#{ingredient}"))
+  			  
+       #    h['hits'].each do |hit| 
+  				 #  text = hit['recipe']['url']
+  				 #  bot.api.send_message(chat_id: message.chat.id, text: text)
+  			  # end            
+  			break
+        end
+  		end
+    
+    end	
   end
 end
 
